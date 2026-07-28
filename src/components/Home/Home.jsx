a23b372244372d155ca8c3d76bcc4eb89cc93a34
Sakhawat Hossain <sakhawath847@gmail.com>
2026-07-29 00:10:37 +0600

    Complete desktop Home section design

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,7 +3,8 @@ import '../../App.css'
 import './Home.css'
 import Profile_pic from '../../assets/images/profile_pic.jpg'
 import { MdEmail } from 'react-icons/md'
-import { FaPhoneAlt, FaLinkedin } from 'react-icons/fa'
+import { FaPhoneAlt, FaLinkedin, FaGithub } from 'react-icons/fa'
+import { FaG } from 'react-icons/fa6'
 
 const Home = () => {
     return (
@@ -24,12 +25,12 @@ const Home = () => {
                                 <h1> Sakhawat Hossain </h1>
                             </div>
                             <div className="designation">
-                                <h2> Junior Full Stack Developer & Amazon PPC Specialist </h2>
+                                <h3> Junior Full Stack Developer & Amazon PPC Specialist </h3>
                             </div>
                             <div className="short-intro">
-                                <p> <p>
+                                <p>
                                     I am a passionate Junior Full Stack Developer who is always eager to learn and grow in software development. Currently, I work as a Junior E-commerce Specialist, managing Amazon PPC campaigns while continuing to improve my web development skills.
-                                </p></p>
+                                </p>
                             </div>
                             <div className="contact-info">
                                 <p>
@@ -42,7 +43,20 @@ const Home = () => {
                                 </p>
                                 <p>
                                     <FaLinkedin className="icon" />
-                                    <a href="https://www.linkedin.com/in/sakhawath847/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+                                    <a href="https://www.linkedin.com/in/sakhawath847/" target="_blank" rel="noopener noreferrer"
+                                        title="Visit in/sakhawath847">
+                                        LinkedIn
+                                    </a>
+                                </p>
+                                <p>
+                                    <FaGithub className="icon" />
+                                    <a
+                                        href="https://github.com/Sakhawat-hossain-369"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        title="Visit Sakhawat-hossain-369"
+                                    >Github
+                                    </a>
                                 </p>
 
                             </div>
